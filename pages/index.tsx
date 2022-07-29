fcc63c4cfc3a1024ee873df1201bff0a73b690ad
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-29 16:12:17 -0500

    feat: built basic layout for homepage with placeholder colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,21 +12,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/book-icon.ico" />
       </Head>
 
+      <header className={styles.header}>
+        <h1>This is the header</h1>
+      </header>
+
+      <aside className={styles.aside}>
+        This is the aside
+      </aside>
+
       <main className={styles.main}>
         
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        This is the footer
       </footer>
     </div>
   )
