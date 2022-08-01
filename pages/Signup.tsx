6249e7dd799a8aa00a4691112263099f235d503a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-01 17:58:07 -0500

    feat: add API fetch request in formSubmitHandler to test route api/hello

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import styles from '../styles/Signup.module.css'
 
 const Signup: NextPage = () => {
 
     // state of form inputs
-    const [ formState, setFormState ] = useState({ email: '', password: '' });
+    const [ formState, setFormState ] = useState({ username: '', email: '', password: '' });
+
+    // router for page refresh/redirect
+    let router = useRouter();
 
     // updates formState with current form input values
     // typescript type set for form event copied from https://stackoverflow.com/questions/42081549/typescript-react-event-types | original authors: cham, Edwin, Nitzan tomer
@@ -19,9 +23,45 @@ const Signup: NextPage = () => {
         });
     };
 
+    const formSubmitHandler = (event: React.FormEvent<EventTarget>): void => {
+        event.preventDefault();
+
+        const { username, email, password } = formState;
+
+        // test for fetch request to API handler in pages/api/hello.ts
+        try {
+            fetch('/api/hello', {
+              method: 'POST',
+              body: JSON.stringify({
+                  username: username,
+                  email: email,
+                  password: password
+              }),
+            })
+            .then(res => {
+              res.json().then(data => console.log(data))
+            });
+        }
+        catch(err) {
+            console.error(err);
+        }
+        finally {
+            setFormState({
+                username: '',
+                email: '',
+                password: ''
+            });
+
+            router.reload();
+        }
+    }
+
     return (
         <section>
-            <form id={'signup-form'} className={styles.form}>
+            <form
+                id={'signup-form'} className={styles.form}
+                onSubmit={formSubmitHandler}
+            >
                 <label htmlFor='username'>Username </label>
                 <input
                     type='text'
@@ -31,7 +71,7 @@ const Signup: NextPage = () => {
                 />
                 <label htmlFor='email'>Email </label>
                 <input
-                    type='text'
+                    type='email'
                     name='email'
                     className={styles.input}
                     onChange={inputChangeHandler}
@@ -47,7 +87,8 @@ const Signup: NextPage = () => {
                 <button
                     type='submit'
                     className={styles.btn}
-                >Sign Up
+                    onClick={formSubmitHandler}
+                >Create Account
                 </button>
             </form>
         </section>
