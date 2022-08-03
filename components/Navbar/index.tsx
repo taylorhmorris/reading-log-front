aaec9373527cbe009857b2fa4417a31a07a0cf2f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-03 01:13:08 -0500

    fix: change Navbar component to typescript

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import styles from '../../styles/Navbar.module.css'
 
 export default function Navbar() {
-
     return (
         <div className={styles.navbar}>
             <Link href='/current'>
