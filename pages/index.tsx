ac1482497f940345b9bf3155c3ec96be46139d00
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-16 17:43:41 -0500

    feat: hardcode test user data in static props on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 
 import Navbar from '../components/Navbar'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+
+  const { apiData } = props;
+
   return (
     <Layout>
       <aside className={styles.aside}>
@@ -12,10 +16,20 @@ const Home: NextPage = () => {
       </aside>
 
       <section className={styles.home}>
-        This is the home page
+        Welcome, {apiData.user}
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const user = 'username';
+  const apiData = { user: user };
+  return {
+    props: {
+      apiData
+    }
+  }
 }
 
 export default Home
