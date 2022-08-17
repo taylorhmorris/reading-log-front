6c1006e90cc9808abacdf16d96602837f2bb057a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-17 02:35:43 -0500

    feat: add BookInfo component to display selected book info

--- pages/books.tsx
+++ pages/books.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import { GetStaticProps } from 'next'
+import { useState } from 'react'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 
 import Navbar from '../components/Navbar'
+import BookInfo from '../components/BookInfo'
 
 const Books: NextPage = (props) => {
     const books = [{
@@ -11,23 +13,26 @@ const Books: NextPage = (props) => {
         title: 'The Hitchhikers Guide to the Galaxy',
         length: 42,
         language: 'English',
-        publicationDate: Date.now(),
+        publicationDate: 1942,
         edition: '1st'
     }, {
         author: 'J.R.R. Tolkien',
         title: 'The Hobbit',
         length: 300,
         language: 'English',
-        publicationDate: Date.now(),
+        publicationDate: 1901,
         edition: '1st'
     }, {
         author: 'David Foster Wallace',
         title: 'Infinite Jest',
         length: 1000000,
         language: 'English',
-        publicationDate: Date.now(),
+        publicationDate: 1990,
         edition: '1st'
     }];
+
+    // const [ currentBook, setCurrentBook ] = useState({});
+    const currentBook = books[0];
 
     return (
         <Layout>
@@ -36,20 +41,25 @@ const Books: NextPage = (props) => {
             </aside>
         
             <section className={styles.books}>
+                <div className={styles.bookList}>
                 {books.map(book => {
                     return (
-                        <div key={book.title} className={styles.bookCard}>
+                        <div key={book.title} className={styles.bookItem}>
                             <p className={styles.bookTitle}>{book.title}</p>
-                            <p className={styles.bookAuthor}>by {book.author}</p>
-                            <ul className={styles.bookInfo}>
-                                <li>Page length: {book.length}</li>
-                                <li>Language: {book.language}</li>
-                                <li>Published on: {book.publicationDate}</li>
-                                <li>Edition: {book.edition}</li>
-                            </ul>
                         </div>
                     )
-                })}              
+                })}
+                </div>  
+                <div>
+                    <BookInfo
+                        title={currentBook.title}
+                        author={currentBook.author}
+                        length={currentBook.length}
+                        language={currentBook.language}
+                        publicationDate={currentBook.publicationDate}
+                        edition={currentBook.edition}
+                    />  
+                </div>        
             </section>
         </Layout>
     )
