72c46ea3a6b702b16d8f8b3f1aaccee4d526c188
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-22 23:43:01 -0500

    feat: add context to current readings page

--- pages/current-readings.tsx
+++ pages/current-readings.tsx
@@ -1,19 +1,46 @@
 import type { NextPage } from 'next'
+import { useContext, useState, useEffect } from 'react'
+import { Context } from '../Context'
+import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 
 import Navbar from '../components/Navbar'
 
 const CurrentReadings: NextPage = () => {
+
+    const [ context, setContext ] = useContext(Context);
+    const loggedIn = context.loggedIn;
+
     return (
         <Layout>
-            <aside className={styles.aside}>
-              <Navbar />
-            </aside>
-        
-            <section className={styles.home}>
-                This is the current readings section
-            </section>
+            {loggedIn ? (
+            <>
+                <aside className={styles.aside}>
+                  <Navbar />
+                </aside>
+                
+                <section className={styles.home}>
+                    {context.userId}
+                </section>
+            </>
+            ) : (
+            <>
+                <section className={styles.home}>
+                  You are not logged in. Please log in, or create an account.
+                  <Link href='/login'>
+                    <a className={styles.navlink}>
+                      Login
+                    </a>
+                  </Link>
+                  <Link href='/signup'>
+                    <a className={styles.navlink}>
+                      Create Account
+                    </a>
+                  </Link>
+                </section>
+            </>
+            )}
         </Layout>
     )
 }
