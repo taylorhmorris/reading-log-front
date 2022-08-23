32f00fe62e7d2d3a45488475990dbd2a10d55edf
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-23 00:15:57 -0500

    feat: add format_date helper to utils/helpers

--- pages/current-readings.tsx
+++ pages/current-readings.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState } from 'react'
 import { Context } from '../Context'
 import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import { format_date } from '../utils/helpers'
 
 import Navbar from '../components/Navbar'
 
@@ -11,6 +12,20 @@ const CurrentReadings: NextPage = () => {
 
     const [ context, setContext ] = useContext(Context);
     const loggedIn = context.loggedIn;
+
+    const [ currentReading, setCurrentReading ] = useState({
+        ownerId: 4,
+        bookId: 1,
+        startDate: Date.now(),
+        book: {
+            author: 'Douglas Adams',
+            title: 'The Hitchhikers Guide to the Galaxy',
+            length: 208,
+            language: 'English',
+            publicationDate: 1979,
+            edition: '1st'
+        }
+    });
 
     return (
         <Layout>
@@ -20,8 +35,28 @@ const CurrentReadings: NextPage = () => {
                   <Navbar />
                 </aside>
                 
-                <section className={styles.home}>
-                    {context.userId}
+                <section className={styles.books}>
+                    <div className={styles.currentReadingInfo}>
+                        <p>Current Progress: <span id='progress'></span></p>
+                        <p>Pages Read Toady: <span id='pagesRead'></span></p>
+                        <p>Today&apos;s date: {format_date(Date.now())}</p>
+                        <label htmlFor='newRead'>How many pages did you read today?</label>
+                        <input 
+                            type='text'
+                            name='newRead'
+                        />
+                        <button>Log Pages</button>
+                    </div>  
+                    <div className={styles.bookInfo}>
+                        <h3>{currentReading.book.title}</h3>
+                        <h4>by {currentReading.book.author}</h4>
+                        <ul>
+                            <li>Total Pages: {currentReading.book.length}</li>
+                            <li>Language: {currentReading.book.language}</li>
+                            <li>Published on: {currentReading.book.publicationDate}</li>
+                            <li>Edition: {currentReading.book.edition}</li>
+                        </ul>
+                    </div> 
                 </section>
             </>
             ) : (
