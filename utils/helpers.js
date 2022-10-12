0198ba76714460ee75f81718aa9b3d43c78e3f79
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-12 00:06:46 -0500

    chore: remove all previous files, keep github workflow

--- utils/helpers.js
+++
@@ -1,8 +0,0 @@
-// format_date function code is copied from https://github.com/strudelAndCoffee/just-tech-news/blob/main/utils/
-function format_date(date) {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
-    ).getFullYear()}`;
-}
-
-export { format_date }
