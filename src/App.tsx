c09f9f4b9ef11c800eb0e4038a1f834f979b4e64
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-13 16:10:23 -0500

    feat: create UserForm component, add header/main/footer tags to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,20 @@ function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/*" element={<NoMatch />} />
-        </Routes>
+        <div>
+          <header>
+            <h1>Header</h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/*" element={<NoMatch />} />
+            </Routes>
+          </main>
+          <footer>Footer</footer>
+        </div>
       </Router>
     </>
   );
