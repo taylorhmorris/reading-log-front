53945c6f655b0078073d00f3856b3ba89c67c578
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-15 16:57:00 -0500

    feat: add current page link style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,38 @@
+import { useEffect } from 'react';
 import styles from '../styles/navbar.module.css';
 
 export function Navbar() {
+  useEffect(() => {
+    let page = window.location.pathname.split('/')[1];
+    if (page === '') page = 'home';
+    const el = document.querySelector(`#${page}`);
+    const current = ' ' + styles.current;
+    if (el) el.className += current;
+  }, []);
+
   return (
     <nav className={styles.navbar}>
-      <a href="/">Home</a>
-      <a href="/login">Log In</a>
-      <a href="/signup">Sign Up</a>
-      <a href="/current-readings">Current Readings</a>
-      <a href="/past-readings">Past Readings</a>
-      <a href="/books">Books</a>
-      <a href="/authors">Authors</a>
+      <a href="/" id="home" className={styles.link}>
+        Home
+      </a>
+      <a href="/login" id="login" className={styles.link}>
+        Log In
+      </a>
+      <a href="/signup" id="signup" className={styles.link}>
+        Sign Up
+      </a>
+      <a href="/current-readings" id="current-readings" className={styles.link}>
+        Current Readings
+      </a>
+      <a href="/past-readings" id="past-readings" className={styles.link}>
+        Past Readings
+      </a>
+      <a href="/books" id="books" className={styles.link}>
+        Books
+      </a>
+      <a href="/authors" id="authors" className={styles.link}>
+        Authors
+      </a>
     </nav>
   );
 }
