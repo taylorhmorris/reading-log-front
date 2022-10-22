e9ce2953bd713bc17498c4cd279a64deb9d3cc02
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-22 15:58:58 -0500

    feat: create UserContext.ts

---
+++ src/context/UserContext.ts
@@ -0,0 +1,14 @@
+import { createContext, useContext, ReactNode } from 'react';
+
+type UserContext = {};
+type ScheduleProviderProps = {
+    children: ReactNode
+};
+
+const UserContext = createContext({} as UserContext);
+
+export function userContext() {
+    return useContext(UserContext)
+};
+
+export function UserProvider({ children }: ScheduleProviderProps) {};
