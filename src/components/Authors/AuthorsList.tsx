fdafb95f44ace2cc2bfd2d03e1a0988b489d4cf8
tmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-11-11 18:36:23 +0100

    feat: update AuthorsList to fetch authors from API

--- src/components/Authors/AuthorsList.tsx
+++ src/components/Authors/AuthorsList.tsx
@@ -1,26 +1,49 @@
+import React from 'react';
 import { IAuthor } from '../../interfaces/IAuthor';
+import AuthorFetchHandler from '../../utils/authorFetchHandler';
 import { AuthorsListItem } from './AuthorsListItem';
 
-export function AuthorsList() {
-  function fetchAuthors(): IAuthor[] {
-    /*
-     *  This function will be replaced by the fetchHandler
-     *  to get authors from the DB
-     */
-    return [
-      { id: 1, lastName: 'Twain', firstNames: 'Mark' },
-      { id: 3, lastName: 'Hemingway', firstNames: 'Ernest' },
-      { id: 2, lastName: 'Stoppard', firstNames: 'Tom' },
-    ];
+export type AuthorsListPropsType = Record<any, never>;
+
+export interface AuthorsListStateType {
+  authors: IAuthor[];
+}
+
+export class AuthorsList extends React.Component<
+  AuthorsListPropsType,
+  AuthorsListStateType
+> {
+  constructor(props: AuthorsListPropsType) {
+    super(props);
+    this.state = {
+      authors: [],
+    };
   }
 
-  const authors = fetchAuthors();
-  authors.sort((a, b) => {
-    return a.lastName.localeCompare(b.lastName);
-  });
-  const listAuthors = authors.map((author) => (
-    <AuthorsListItem key={author.id} author={author} />
-  ));
+  fetchAuthors() {
+    AuthorFetchHandler.get_authors()
+      .then((authors: IAuthor[]) => {
+        authors.sort((a: IAuthor, b: IAuthor) => {
+          return a.lastName.localeCompare(b.lastName);
+        });
+        this.setState(() => ({
+          authors: authors,
+        }));
+      })
+      .catch((error) => {
+        console.error(`Could not fetch authors: ${error}`);
+        return [];
+      });
+  }
 
-  return <div>{listAuthors}</div>;
+  componentDidMount(): void {
+    this.fetchAuthors();
+  }
+
+  render() {
+    const listAuthors = this.state.authors.map((author) => (
+      <AuthorsListItem key={author.id} author={author} />
+    ));
+    return <div>{listAuthors}</div>;
+  }
 }
