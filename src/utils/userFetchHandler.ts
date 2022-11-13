f330993cb663e57cd278ff1331e070b346f349ea
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-12 19:41:23 -0600

    refactor: wrap reqs in UserFetchHandler in try/catch statements

--- src/utils/userFetchHandler.ts
+++ src/utils/userFetchHandler.ts
@@ -5,31 +5,38 @@ const api_url: string | undefined | null = import.meta.env.VITE_API_URL;
 class UserFetchHandler {
   async signup(formData: FormData) {
     const url = api_url + '/users';
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
 
-    if (response.ok) {
-      const login_res = await this.login(formData);
-      if (login_res) return login_res;
+    try {
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }).then((res) => {
+        if (res.ok) {
+          return this.login(formData);
+        }
+      });
+    } catch (err) {
+      console.error(err);
     }
-    return response;
   }
   async login(formData: FormData) {
     const url = api_url + '/auth/login';
-    const response = fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    }).then((res) => res.json());
+    try {
+      const response = fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }).then((res) => res.json());
 
-    return response;
+      return response;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
