d1f415812cc157236bd5dabf1e0b360f8073291b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-16 18:44:51 -0600

    feat: add QueryClientProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { UserContextProvider } from './context/UserContext';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
